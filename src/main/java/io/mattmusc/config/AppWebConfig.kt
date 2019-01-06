b0dea3019ebada401b529fd2a0238b45f376c478
Matteo Muscella <matteo.muscella@mail.com>
2019-01-06 17:31:20 +0100

    Adds Angular front end framework.

--- src/main/java/io/mattmusc/config/AppWebConfig.kt
+++ src/main/java/io/mattmusc/config/AppWebConfig.kt
@@ -2,9 +2,11 @@ package io.mattmusc.config
 
 import org.springframework.context.ApplicationContext
 import org.springframework.context.ApplicationContextAware
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.thymeleaf.spring5.ISpringTemplateEngine
@@ -13,9 +15,16 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver
 import org.thymeleaf.spring5.view.ThymeleafViewResolver
 import org.thymeleaf.templatemode.TemplateMode
 import org.thymeleaf.templateresolver.ITemplateResolver
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
 
 @EnableWebMvc
 @Configuration
+@EnableSwagger2
 @ComponentScan("io.mattmusc.controller")
 open class ApplicationWebConfig : ApplicationContextAware, WebMvcConfigurer {
 
@@ -29,6 +38,23 @@ open class ApplicationWebConfig : ApplicationContextAware, WebMvcConfigurer {
     override fun configureViewResolvers(registry: ViewResolverRegistry) {
         registry.viewResolver(ThymeleafViewResolver()
                 .apply { templateEngine = templateEngine() })
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry?) {
+        registry?.addResourceHandler("swagger-ui.html")
+                ?.addResourceLocations("classpath:/META-INF/resources/")
+
+        registry?.addResourceHandler("/webjars/**")
+                ?.addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+
+    @Bean
+    open fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
     }
 
     private fun templateEngine(): ISpringTemplateEngine? {
--- src/main/java/io/mattmusc/controller/HelloController.kt
+++ src/main/java/io/mattmusc/controller/HelloController.kt
@@ -3,15 +3,17 @@ package io.mattmusc.controller
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.view.RedirectView
+import java.net.http.HttpResponse
 
 @Controller
 class MyController {
     private val logger = LoggerFactory.getLogger(MyController::class.java)
 
     @GetMapping("/")
-    fun home(model: Model): String {
-        model.addAttribute("message", "Hello from Kotlin and Spring MVC 5 - Hey!")
-        return "index"
+    fun home(): RedirectView {
+        return RedirectView("swagger-ui.html")
     }
 }
