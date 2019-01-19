d6809d132cb7ec1c892337b66ae16d0943a9ca8e
Matteo Muscella <matteo.muscella@mail.com>
2019-01-20 00:10:05 +0100

    Adds mock data, set up maven filters to replace properties in liquibase config file.

---
+++ backend/src/test/java/io/mattmusc/converters/DoubleAttributeConverterTest.kt
@@ -0,0 +1,30 @@
+package io.mattmusc.converters
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+
+internal class DoubleAttributeConverterTest
+{
+	private val converter = DoubleAttributeConverter()
+
+	@Test
+	fun convertToDatabaseColumn()
+	{
+		Assertions.assertNull(converter.convertToDatabaseColumn(null))
+
+		val num2convert = converter.convertToDatabaseColumn(5.0)
+		Assertions.assertNotNull(num2convert)
+		Assertions.assertEquals(BigDecimal(5.0), num2convert)
+	}
+
+	@Test
+	fun convertToEntityAttribute()
+	{
+		Assertions.assertNull(converter.convertToEntityAttribute(null))
+
+		val num2convert = converter.convertToEntityAttribute(BigDecimal(5.0))
+		Assertions.assertNotNull(num2convert)
+		Assertions.assertEquals(5.0, num2convert)
+	}
+}
